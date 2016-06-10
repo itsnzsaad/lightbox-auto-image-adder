596d15b173e615bf783798e77e35652728d6d776
itsnzsaad ‍‍‍ <itsnzsaad >
2016-06-10 15:45:12 +0600

    Yoo

---
+++ demo/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Lightbox Auto Image Adder</title>
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+	<!-- Stylesheet Call -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">  
+	<!-- Stylesheet Call -->
+
+
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	</head>
+
+<body>
+
+<div class="ins">
+  <h1>Lightbox Auto Image Adder</h1>
+  <h2>How To Use?</h2>
+  <p>
+    <img src="images/1.png" alt=""><br>
+    <img src="images/2.png" alt=""><br>
+    <img src="images/3.png" alt=""><br>
+    <img src="images/4.png" alt=""><br>
+  </p>
+</div>
+
+
+<h1>Demo:</h1>
+
+
+<div class="lb_gallery">
+		<?php
+      $Folder_Name = "lightbox_images";
+
+      $images = glob("$Folder_Name/*.{jpg,png,gif}", GLOB_BRACE);
+
+
+      foreach($images as $image){
+
+  					echo '<a href="'.$image.'" data-lightbox="auto_lightbox">
+  					<img class="lb_img" src="' .$image. '" alt="">
+  					</a>';
+
+  			}
+    ?>
+</div>
+
+
+</body>
+
+<!-- =========================================  
+====================== /Gallery ==============
+========================================   -->
+
+  <!-- jQuery Call -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script type="text/javascript" 
+  src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js">    
+  </script>
+  <!-- jQuery Call -->
+
+</html>
